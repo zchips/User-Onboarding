7132846e13400eafdb45c4b71ce987fdfabcba7c
zchips <zchips@gmail.com>
2023-05-20 15:23:20 -0600

    pushing random modifications, eek

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Form from './Components/Form';
+
+
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+      <Form/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ user-onboarding/src/Components/Form.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Form = (props) => {
+    return (
+     <h1> Starting the form</h1>
+    )
+}
+
+export default Form
