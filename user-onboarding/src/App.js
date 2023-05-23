805ea466807964e56025598fda77a258e30187d0
zchips <zchips@gmail.com>
2023-05-23 17:19:17 -0600

    push before scheme

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useState} from 'react'
 import Form from './Components/Form';
 
 
+const initialFormValues = {
+  username: '',
+  password:'',
+  email:  '',
+  tos: false
+}
+
+
 function App() {
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const clickHandler = (name, value) => {
+    setFormValues({... formValues, [name]: value})
+  }
+
   return (
     <div className='App'>
-      <Form/>
+      <Form values= {initialFormValues} onChange={clickHandler}/>
        
     </div>
   );
--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,40 +1,55 @@
 import React from 'react'
 
 const Form = (props) => {
+    const { change, submit } = props
+    const {username, email, password, tos} = props.value
+    
+  const onChange = (e) => {
+        const {name, value, checked, type} = e.target
+        const newVal = type === 'checkbox'? checked: value
+        change(name, newVal)
+    }
+   const onSubmit = (e) => {
+        e.preventDefault()
+        submit()
+    }
+
+
+    
     return (
      <div>
      <h1> Starting the form</h1>
-     <form>
-        <label> Name:
-            <input type="text"
-                    name="name"
-                    value={name}
-                    onChange={onChange}
-           />
-        </label>
-        <label> Email:
-            <input type="email"
-                    name="email"
-                    value={email}
-                    onChange={onChange}
-           />
-        </label>
-        <label> Password:
-            <input type="password"
-                    name="password"
-                    value={password}
-                    onChange={onChange}
-           />
-        </label>
-        <label> Terms of Service:
-            <input type="checkbox"
-                    name="tos"
-                    checked={checked}
-                    onChange={onChange}
-           />
-        </label>
+     <form onSubmit = {onSubmit}>
+                        <label> Name:
+                            <input type="text"
+                                    name="username"
+                                    value={username}
+                                    onChange={onChange}
+                        />
+                        </label>
+                        <label> Email:
+                            <input type="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={onChange}
+                        />
+                        </label>
+                        <label> Password:
+                            <input type="password"
+                                    name="password"
+                                    value={password}
+                                    onChange={onChange}
+                        />
+                        </label>
+                        <label> Terms of Service:
+                            <input type="checkbox"
+                                    name="tos"
+                                    checked={tos}
+                                    onChange={onChange}
+                        />
+                        </label>
         
-        <input type="submit" value="Create a Friend!" 
+        <input type="submit" value="Create a Friend!" />
      </form>
 
      </div>
